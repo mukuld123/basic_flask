83357fa7eeba4a95bf4c73c7d46e8411f9e87a31
Mukul Dixit <mukul.d@consultadd.com>
2023-07-10 15:08:19 +0530

    added templates and routes for login and logout

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from forms import *
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '8c1370dfb8d4967de99dbfb8031fc05c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return "Hello"
+    return render_template('index.html')
 
 @app.route('/code')
 def code():
-    return "route for code"
+    return render_template('code.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate():
+        # print('validated')
+        return redirect(url_for('login'))
+    return render_template('register.html', form = form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate():
+        # print('validated')
+        return redirect(url_for('home'))
+    return render_template('login.html', form = form)
 
 
 if __name__ == '__main__':
