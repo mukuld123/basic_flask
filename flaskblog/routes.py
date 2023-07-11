16a2ef366760e8641e8954fc9f5cbfad70e3ee5c
Mukul Dixit <mukul.d@consultadd.com>
2023-07-11 16:51:34 +0530

    code restructure and authentication

---
+++ flaskblog/routes.py
@@ -0,0 +1,56 @@
+from flask import render_template, flash, redirect, url_for
+from flaskblog import app, db, bcrypt
+from flaskblog.models import User, Post
+from flaskblog.forms import RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/code')
+def code():
+    return render_template('code.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate():
+        # print('validated')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', form = form)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Enter correct username and password')
+        # print('validated')
+            return redirect(url_for('login'))
+        while True:
+            pass
+    return render_template('login.html', form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html')
